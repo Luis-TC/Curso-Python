6a5cd081a86280a903073cc3109818ada087a0a9
Luis-TC <luistobo2015@gmail.com>
2024-11-09 09:47:07 -0500

    Ejemplo Codicionales

---
+++ Clase 4/Ejercicio Condicionales.py
@@ -0,0 +1,60 @@
+"""# 1) Comparar dos números y determinar si son iguales.
+
+num_1=int(input("Ingresa primer número a comparar: "))
+num_2=int(input("Ingresa segundo número a comparar: "))
+
+if  num_1==num_2:
+    print("Los números", num_1, "y", num_2, "son iguales")
+else:
+    print("Los números", num_1, "y", num_2, "no son iguales")
+"""
+
+"""# 2) Identificar cuál de dos números es el mayor.
+
+num_1=int(input("Ingresa primer número a comparar: "))
+num_2=int(input("Ingresa segundo número a comparar: "))
+
+if  num_1<num_2:
+    print("El número mayor es: ", num_2)
+else:
+    print("El número mayor es: ", num_1)
+
+"""
+# 3) Decide si un número es par o impar.
+
+num_1=int(input("Ingresa el número a definri si es par o impar: "))
+
+if num_1 % 2 == 0:
+       print("El número", num_1,"es Par")
+else:
+        print("El número", num_1,"es Impar")
+
+
+
+# 4) Clasificador de triángulos: Clasifica triángulos según sus lados.
+
+Euilatero
+Isosceles
+Escaleno
+
+print("Clasifi")
+num_1=int(input("Ingresa primer número a comparar: "))
+num_2=int(input("Ingresa segundo número a comparar: "))
+num_3=int(input("Ingresa segundo número a comparar: "))
+
+if  num_1==num_2:
+    print("Los números", num_1, "y", num_2, "son iguales")
+else:
+    print("Los números", num_1, "y", num_2, "no son iguales")
+
+# 5) Edad de jubilación: Determina si una persona puede jubilarse. 
+
+
+
+# 6) Dia laboral: Determina si es dia laboral o fin de semana.
+
+Dia= input('ingrese Dia:')
+print('ingresaste el dia:',Dia)
+
+if Dia in ["lunes", "martes", "miércoles", "miercoles", "jueves", "viernes"]:print('es dia laboral')
+else: print('no es dia laboral')
