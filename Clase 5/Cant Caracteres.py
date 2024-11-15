5c8b0971ac6c354b637f19a3c61662886297d992
Luis-TC <luistobo2015@gmail.com>
2024-11-14 21:40:44 -0500

    Create Cant Caracteres.py

---
+++ Clase 5/Cant Caracteres.py
@@ -0,0 +1,6 @@
+# caracteres del nombre
+Nombre =input("ingrese su nombre: ")
+contador = 0
+for letra in Nombre:
+    contador= contador + 1
+print('el número de caracteres que tiene su nombre son:',contador)
